ea9e227c0d39e72bb90168aac715e388b478525c
rayllanderson <rayllanderson@gmail.com>
2020-11-04 07:58:15 -0300

    criando um pequeno teste junit

---
+++ src.testes/db/teste/InsertTest.java
@@ -0,0 +1,12 @@
+package db.teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InsertTest {
+
+    @Test
+    public void testeConfiguracao() {
+	Assert.assertEquals(2, (1 + 1), 0);
+    }
+}
