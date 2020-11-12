75eecd35e227f709512dd12d2041eed463087dae
rayllanderson <rayllanderson@gmail.com>
2020-11-12 20:42:35 -0300

    refactor: caneca service e imageService

--- src/com/ray/model/service/CanecaService.java
+++ src/com/ray/model/service/CanecaService.java
@@ -1,19 +1,21 @@
 package com.ray.model.service;
 
-import java.util.List;
-
 import com.ray.model.dao.CanecaRepository;
+import com.ray.model.dao.ImageRepository;
 import com.ray.model.dao.RepositoryFactory;
+import com.ray.model.entities.Arquivo;
 import com.ray.model.entities.Caneca;
 
 public class CanecaService {
     
     private CanecaRepository canecaRepository; 
     private ImageService imageService;
+    private ImageRepository imgRepository;
     
     public CanecaService() {
 	this.canecaRepository = RepositoryFactory.createCanecaDao();
 	this.imageService = new ImageService();
+	this.imgRepository = RepositoryFactory.createImageDao();
     }
     
     public Caneca save(Caneca caneca) {
@@ -26,38 +28,20 @@ public class CanecaService {
     
     /**
      * Recupera todas as canecas do cliente, verifica se a caneca de fato pertence ao cliente, se pertencer, deleta e retorna true, senao, retorna false
-     * Nota: Este método também exlui a imagem da caneca
+     * Nota: Este método também exlui todas imagens da caneca
      * @param id- id da caneca
      * @param clientId - id do cliente para verificar se a caneca pertence ao cliente
      */
     public boolean deleteById(Long id, Long clientId) {
 	for(Caneca c : canecaRepository.findAll(clientId)) {
 	    if (c.getId().equals(id)) {
+		for(Arquivo image : imgRepository.findAll(c.getId())) {
+		    imageService.deleteById(image.getId());
+		}
 		canecaRepository.deleteById(id);
-		imageService.deleteById(c.getImage().getId());
 		return true;
 	    }
 	}
 	return false;
-    }
-    
-    /**
-     * 
-     * @param clienteId - todas as canecas de acordo com o id do cliente
-     * @param withInputStream - <br>setar true para caso queira a lista completa, com todos os atributos. <br>Setar falso caso queira a lista parcialmente completa, sem inputstream, base64 e contentType
-     * @return todas as canecas
-     */
-    public List<Caneca> findAll(Long clienteId, boolean withInputStream){
-	List<Caneca> canecas = canecaRepository.findAll(clienteId);
-	if(withInputStream) {
-	    return canecas;
-	}
-	
-	for(Caneca c : canecas) {
-	    c.getImage().setInputStream(null);
-	    c.getImage().setBase64(null);
-	    c.getImage().setContentType(null);
-	}
-	return canecas;
     }
 }
--- src/com/ray/model/service/ImageService.java
+++ src/com/ray/model/service/ImageService.java
@@ -1,5 +1,7 @@
 package com.ray.model.service;
 
+import java.util.List;
+
 import com.ray.model.dao.ImageRepository;
 import com.ray.model.dao.RepositoryFactory;
 import com.ray.model.entities.Arquivo;
@@ -25,5 +27,23 @@ public class ImageService {
 	    repository.deleteById(id);
 	}
     }
-
+    
+    /**
+     * 
+     * @param canecaId - todas as imagens de acordo com o id da caneca
+     * @param withInputStream - <br>setar true para caso queira a lista completa, com todos os atributos. <br>Setar falso caso queira a lista parcialmente completa, sem inputstream, base64 e contentType
+     * @return todas as canecas
+     */
+    public List<Arquivo> findAll(Long canecaId, boolean fullList){
+	List<Arquivo> imagens = repository.findAll(canecaId);
+	if (fullList) {
+	    return imagens;
+	}
+	for(Arquivo image : imagens) {
+	    image.setInputStream(null);
+	    image.setBase64("");
+	    image.setContentType("");
+	}
+	return imagens;
+    }
 }
